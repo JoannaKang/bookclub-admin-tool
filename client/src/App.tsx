2af0c4ef2b71607257e095ec40f23d562c8c3f29
Sooyeon Kang <jfrences11@gmail.com>
2021-11-26 23:15:36 +0000

    feat: browser router added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,23 @@
-import { Wrapper } from './styled';
-import React from 'react'
+import React, {Fragment} from 'react'
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+
+import { Review } from './Pages/Review/Review'
+import { Login } from './Pages/Login/Login'
 
 const App:React.FC = () => {
   return (
-    <Wrapper className="App">
-      <input type="text"/>
-      <input type="button" value="submit"/>
-    </Wrapper>
+    <BrowserRouter>
+      <Fragment>
+        <Routes>
+          <Route path="/" element={<Review />}/>
+          <Route path='/login' element={<Login />} />
+        </Routes>
+      </Fragment>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/Pages/Review/Review.tsx
@@ -0,0 +1,10 @@
+import { Wrapper } from './Style/styled';
+
+export const Review = () => {
+  return (
+    <Wrapper className="Review">
+      <input type="text"/>
+      <input type="button" value="submit"/>
+    </Wrapper>
+  )
+}
