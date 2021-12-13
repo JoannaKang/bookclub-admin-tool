74be00b472c7b90e0d0cc57b2f25ae7f1c52c53c
Joanna Kang <jfrences11@gmail.com>
2021-12-13 23:59:17 +0000

    feat: add sql table create script

---
+++ server/createDB.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql2/promise');
+
+const dbName = "bookclub";
+
+password=process.argv[2]
+
+mysql.createConnection({
+    host: process.env.DB_HOST || "127.0.0.1",
+    port: process.env.DB_PORT || "3306",
+    user: process.env.DB_USER || "root",
+    password : process.env.DB_PASSWORD || password,
+}).then( connection => {
+    connection.query(`CREATE DATABASE IF NOT EXISTS ${dbName};`).then((res) => {
+        console.info("Database create or successfully checked");
+        process.exit(0);
+    })
+})
